d068830905b06a692651965ad3cda64e6fa76a87
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2024-06-28 18:56:23 +0200

    Final commit

---
+++ Zadanie3ProgramowanieObiektowe/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Zadanie3ProgramowanieObiektowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzenie wektorów za pomocą różnych konstruktorów
+            Wektor v1 = new Wektor(3);  // Wektor zerowy o wymiarze 3
+            Wektor v2 = new Wektor(1.0, 2.0, 3.0);  // Wektor o współrzędnych (1, 2, 3)
+            Wektor v3 = new Wektor(4.0, 5.0, 6.0);  // Wektor o współrzędnych (4, 5, 6)
+
+            // Ustawianie wartości za pomocą indeksatora
+            v1[0] = 1.0;
+            v1[1] = 1.0;
+            v1[2] = 1.0;
+
+            // Wyświetlanie współrzędnych i wymiaru wektora
+            Console.WriteLine("Wektor v1:");
+            for (byte i = 0; i < v1.Wymiar; i++)
+            {
+                Console.WriteLine($"v1[{i}] = {v1[i]}");
+            }
+            Console.WriteLine($"Wymiar: {v1.Wymiar}");
+            Console.WriteLine($"Długość: {v1.Dlugosc}");
+            Console.WriteLine();
+
+            // Operacje na wektorach
+            Wektor suma = v2 + v3;
+            Wektor roznica = v3 - v2;
+            Wektor iloczynSkalar = v2 * 2;
+            Wektor iloczynSkalarOdwrotny = 2 * v2;
+            Wektor ilorazSkalar = v2 / 2;
+
+            // Wyświetlanie wyników operacji
+            Console.WriteLine("Suma v2 + v3:");
+            for (byte i = 0; i < suma.Wymiar; i++)
+            {
+                Console.WriteLine($"suma[{i}] = {suma[i]}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Różnica v3 - v2:");
+            for (byte i = 0; i < roznica.Wymiar; i++)
+            {
+                Console.WriteLine($"roznica[{i}] = {roznica[i]}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Iloczyn skalarny v2 * 2:");
+            for (byte i = 0; i < iloczynSkalar.Wymiar; i++)
+            {
+                Console.WriteLine($"iloczynSkalar[{i}] = {iloczynSkalar[i]}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Iloraz skalarny v2 / 2:");
+            for (byte i = 0; i < ilorazSkalar.Wymiar; i++)
+            {
+                Console.WriteLine($"ilorazSkalar[{i}] = {ilorazSkalar[i]}");
+            }
+            Console.WriteLine();
+
+            // Iloczyn skalarny dwóch wektorów
+            double? iloczyn = Wektor.IloczynSkalarny(v2, v3);
+            Console.WriteLine($"Iloczyn skalarny v2 i v3: {iloczyn}");
+        }
+    }
+}
---
+++ Zadanie3ProgramowanieObiektowe/Wektor.cs
@@ -0,0 +1,134 @@
+﻿namespace Zadanie3ProgramowanieObiektowe;
+
+public class Wektor
+{
+    private double[] _wspolrzedne;
+
+    // Konstruktory
+    public Wektor(byte wymiar)
+    {
+        _wspolrzedne = new double[wymiar];
+    }
+
+    public Wektor(params double[] wspolrzedne)
+    {
+        this._wspolrzedne = wspolrzedne;
+    }
+
+    // Właściwości
+    public byte Wymiar
+    {
+        get
+        {
+            return (byte)_wspolrzedne.Length;
+        }
+    }
+
+    public double Dlugosc
+    {
+        get
+        {
+            return Math.Sqrt(IloczynSkalarny(this, this).GetValueOrDefault());
+        }
+    }
+
+    // Indeksator
+    public double this[byte i]
+    {
+        get { return _wspolrzedne[i]; }
+        set { _wspolrzedne[i] = value; }
+    }
+
+    // Statyczne metody
+    public static double? IloczynSkalarny(Wektor v, Wektor w)
+    {
+        if (v.Wymiar != w.Wymiar)
+        {
+            throw new ArgumentException("Wymiary są różne !!");
+        }
+
+        double wynik = 0;
+        for (int i = 0; i < v._wspolrzedne.Length; i++)
+        {
+            wynik += v._wspolrzedne[i] * w._wspolrzedne[i];
+        }
+
+        return wynik;
+    }
+
+    public static Wektor Suma(params Wektor[] wektory)
+    {
+        byte wymiar = wektory[0].Wymiar;
+        foreach (var wektor in wektory)
+        {
+            if (wektor.Wymiar != wymiar)
+            {
+                throw new ArgumentException("Wszystkie wektory muszą mieć ten sam wymiar.");
+            }
+        }
+
+        Wektor wynik = new Wektor(wymiar);
+        for (int i = 0; i < wymiar; i++)
+        {
+            foreach (var wektor in wektory)
+            {
+                wynik._wspolrzedne[i] += wektor._wspolrzedne[i];
+            }
+        }
+
+        return wynik;
+    }
+
+    // Operatory
+    public static Wektor operator +(Wektor v, Wektor w)
+    {
+        return Suma(v, w);
+    }
+
+    public static Wektor operator -(Wektor v, Wektor w)
+    {
+        if (v.Wymiar != w.Wymiar)
+        {
+            throw new ArgumentException("Wymiary są różne !!");
+        }
+
+        Wektor roznica = new Wektor(v.Wymiar);
+        for (int i = 0; i < v.Wymiar; i++)
+        {
+            roznica._wspolrzedne[i] = v._wspolrzedne[i] - w._wspolrzedne[i];
+        }
+
+        return roznica;
+    }
+
+    public static Wektor operator *(Wektor v, double skalar)
+    {
+        Wektor wynik = new Wektor(v.Wymiar);
+        for (int i = 0; i < v.Wymiar; i++)
+        {
+            wynik._wspolrzedne[i] = v._wspolrzedne[i] * skalar;
+        }
+        return wynik;
+    }
+
+    public static Wektor operator *(double skalar, Wektor v)
+    {
+        return v * skalar;
+    }
+
+    public static Wektor operator /(Wektor v, double skalar)
+    {
+        if (skalar == 0)
+        {
+            throw new DivideByZeroException("Nie dzieli się przez zero.");
+        }
+
+        Wektor wynik = new Wektor(v.Wymiar);
+        for (int i = 0; i < v.Wymiar; i++)
+        {
+            wynik._wspolrzedne[i] = v._wspolrzedne[i] / skalar;
+        }
+
+        return wynik;
+    }
+}
